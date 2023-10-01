4bb6e0789a76590ae3e8cfb869c173e71dd4ea4b
venudinesh <venudinesh21@gmail.com>
2023-10-01 23:48:12 +0530

    Ancient bringup

--- device.mk
+++ device.mk
@@ -15,6 +15,11 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
 # Project ID Quota
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+=======
+$(call inherit-product-if-exists, packages/apps/prebuilt-apps/prebuilt-apps.mk)
+
+# Shipping API level
+PRODUCT_SHIPPING_API_LEVEL := 30
 
 # AAPT
 PRODUCT_AAPT_CONFIG := normal
@@ -252,10 +257,10 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-ancient
 
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-ancient
 
 PRODUCT_PACKAGES += \
     WifiOverlay \
@@ -405,6 +410,12 @@ PRODUCT_PACKAGES += \
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator-service.mediatek
+
+# Viper4Android
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilt/viper/lib/soundfx/libv4a_re.so:$(TARGET_COPY_OUT_VENDOR)/lib/soundfx/libv4a_re.so \
+    $(LOCAL_PATH)/prebuilt/viper/lib64/soundfx/libv4a_re.so:$(TARGET_COPY_OUT_VENDOR)/lib/soundfx/libv4a_re.so \
+    $(LOCAL_PATH)/prebuilt/viper/etc/audio_effects.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/audio_effects.conf
 
 # VNDK
 PRODUCT_PACKAGES += \
