75bb76e7814bf3338c112bbe39d0029bdf331d42
venudinesh <venudinesh21@gmail.com>
2023-10-02 10:08:05 +0530

    nashc:  Add script for exTHmUI's GameMode performance control [kanged from violet]

--- device.mk
+++ device.mk
@@ -356,6 +356,7 @@ PRODUCT_PACKAGES += \
     init.mt6785.usb.rc \
     init.nfc_detect.rc \
     init.project.rc \
+	init.performance_level.rc \
     init.sensor_1_0.rc \
     init.target.rc \
     ueventd.mtk.rc \
